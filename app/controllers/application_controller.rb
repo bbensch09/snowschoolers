b3eeba3b54004236b41abf1a57745d74ec8fa32b
Shiv Kumar <shiv@newrelic.com>
2014-02-08 14:42:35 -0800

    move create_lesson method to ApplicationController so it's global

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(user)
+    if params[:user][:date] && params[:user][:slot]
+      params[:lesson_time] = { date: params[:user][:date], slot: params[:user][:slot] }
+      lesson = create_lesson
+      flash[:notice] = "Your <a href='#{lesson_path(lesson)}'>lesson</a> request is being processed.".html_safe
+    end
+    stored_location_for(user) || signed_in_root_path(user)
+  end
+
+  private
+
+  def create_lesson
+    lesson = Lesson.new
+    lesson.lesson_time = LessonTime.find_or_create_by(lesson_time_params)
+    lesson.student = current_user
+    lesson.save
+    return lesson
+  end
+
+  def lesson_time_params
+    params.require(:lesson_time).permit(:date, :slot)
+  end
 end
--- app/controllers/lesson_times_controller.rb
+++ app/controllers/lesson_times_controller.rb
@@ -5,25 +5,11 @@ class LessonTimesController < ApplicationController
 
   def create
     if current_user
-      lesson = create_lesson(params)
+      lesson = create_lesson
       flash[:notice] = "Your <a href='#{lesson_path(lesson)}'>lesson</a> request is being processed.".html_safe
     else
       flash[:alert] = "Please log in to request a lesson."
     end
     redirect_to root_path
   end
-
-  private
-
-    def create_lesson(params)
-      lesson = Lesson.new
-      lesson.lesson_time = LessonTime.find_or_create_by(lesson_time_params)
-      lesson.student = current_user
-      lesson.save
-      return lesson
-    end
-
-    def lesson_time_params
-      params.require(:lesson_time).permit(:date, :slot)
-    end
 end
