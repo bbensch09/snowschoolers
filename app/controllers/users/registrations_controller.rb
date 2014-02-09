4940b016e2819f3e9b7ec1887a3bccfba95c2ec0
Shiv Kumar <shiv@newrelic.com>
2014-02-08 16:53:10 -0800

    move additional user info (outside of email/password) to user edit page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_filter :configure_permitted_parameters, :only => [:create]
+  before_filter :configure_permitted_parameters
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :instructor) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :instructor, :password, 
+                                                                   :password_confirmation, :current_password) }
   end
 end 
