7258b6a2bf5926d26f6dc20eb1610cbe4231dcf0
Shiv Kumar <shiv@newrelic.com>
2014-02-09 12:32:06 -0800

    refactor set_instructor action

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,13 +1,21 @@
 class LessonsController < ApplicationController
+
   def show
     @lesson = Lesson.find(params[:id])
     @lesson_time = @lesson.lesson_time
   end
 
   def set_instructor
-    lesson = Lesson.find(params[:id])
-    lesson.instructor = current_user
-    lesson.save
-    redirect_to lesson
+    set_lesson_instructor
+    redirect_to @lesson
+  end
+
+  private
+
+
+  def set_lesson_instructor
+    @lesson = Lesson.find(params[:id])
+    @lesson.instructor = current_user
+    @lesson.save
   end
 end
